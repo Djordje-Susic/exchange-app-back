05eb4b860e1816511c644eeef63a61cd3198cf6b
Djole <djordje.susic@gmail.com>
2022-04-16 17:38:11 +0200

    feat: in ExchangeQuote model added 'quote' calculated attribute

--- app/Models/ExchangeQuote.php
+++ app/Models/ExchangeQuote.php
@@ -18,5 +18,28 @@ class ExchangeQuote extends Model
         'send_email',
         'updated_at',
         'created_at',
+        'exchange_rate',
+        'surcharge_percentage'
+
     ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'quote',
+    ];
+
+    public function getQuoteAttribute(){
+        $quote =
+            $this->exchange_rate
+            // Apply surcharge
+            * (1 + $this->surcharge_percentage / 100)
+            // Apply discount
+            * (1 - $this->discount_percentage / 100);
+
+        return round($quote, 6);
+    }
 }
